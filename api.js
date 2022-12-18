79b6c40b4586351ccae526de65a81dc12b95645a
16bitash <entropy.ash@gmail.com>
2022-12-18 23:05:18 +0530

    Complete promises

--- api.js
+++ api.js
@@ -1,24 +1,51 @@
-function convertToJSON(response) {
-  return response.json();
-}
+// function convertToJSON(response) {
+//   return response.json();
+// }
 
-function logData(data) {
+// function logData(data) {
+//   console.log(data);
+// }
+
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//   method: "GET",
+// })
+//   .then(convertToJSON)
+//   .then(logData);
+
+const getPosts = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  const data = await response.json();
+
   console.log(data);
-}
+};
 
-fetch("https://jsonplaceholder.typicode.com/posts", {
-  method: "GET",
-})
-  .then(convertToJSON)
-  .then(logData);
+getPosts();
 
-fetch("https://jsonplaceholder.typicode.com/posts", {
-  method: "POST",
-  body: {
-    title: "foo",
-    body: "bar",
-    userId: 1,
-  },
-})
-  .then(convertToJSON)
-  .then(logData);
+// fetch("https://jsonplaceholder.typicode.com/posts", {
+//   method: "POST",
+//   body: {
+//     title: "foo",
+//     body: "bar",
+//     userId: 1,
+//   },
+// })
+//   .then(convertToJSON)
+//   .then(logData);
+
+const createPost = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: {
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    },
+  });
+
+  const data = await response.json();
+
+  console.log(data);
+};
+
+createPost();
--- promises.js
+++ promises.js
@@ -1,25 +1,71 @@
 const wait = (ms) => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
+    //   resolve("done");
       resolve();
+    //   reject("Internet issue");
+    //   reject();
     }, ms);
   });
 
   return promise;
 };
 
-setTimeout(() => {
-  console.log("Hello");
+// setTimeout(() => {
+//   console.log("Hello");
 
-  setTimeout(() => {
+//   setTimeout(() => {
+//     console.log("World");
+
+//     setTimeout(() => {
+//       console.log("Goodbye");
+
+//       setTimeout(() => {
+//         console.log("Hi again");
+//       }, 1000);
+//     }, 3000);
+//   }, 1000);
+// }, 2000);
+
+// wait(2000)
+//   .then(() => {
+//     console.log("Hello");
+
+//     return wait(1000);
+//   })
+//   .then(() => {
+//     console.log("World");
+
+//     return wait(3000);
+//   })
+//   .then(() => {
+//     console.log("Goodbye");
+
+//     return wait(1000);
+//   })
+//   .then(() => {
+//     console.log("Hi again");
+//   })
+//   .catch(() => {
+//     console.log("Error");
+//   });
+
+const run = async () => {
+  try {
+    await wait(2000);
+    console.log("Hello");
+
+    await wait(1000);
     console.log("World");
 
-    setTimeout(() => {
-      console.log("Goodbye");
+    await wait(3000);
+    console.log("Goodbye");
 
-      setTimeout(() => {
-        console.log("Hi again");
-      }, 1000);
-    }, 3000);
-  }, 1000);
-}, 2000);
+    await wait(1000);
+    console.log("Hi again");
+  } catch {
+    console.log("Error");
+  }
+};
+
+run();
